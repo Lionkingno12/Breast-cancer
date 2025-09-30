fcc1d6eefc2e2dba1816d60af34437c753ae96c8
Lionkingno12 <astitvath9@gmail.com>
2025-09-30 13:56:04 +0530

    bug fix

--- model_traning.py
+++ model_traning.py
@@ -1,7 +1,7 @@
 import numpy as np
 import copy
 x_axis=np.array([[2],[15],[20],[49],[59],[78],[61],[43],[49],[56]])
-y_axis=np.array([0,0,0,0,1,1,1,1,0,1,1])
+y_axis=np.array([0,0,0,0,1,1,1,1,0,1])
 w=np.zeros(x_axis.shape[1])
 # sigmoid function to convert the lenear to more better graph
 def sigmoid(z):
@@ -9,7 +9,7 @@ def sigmoid(z):
     return g
 # lenear equation
 def func(w,b):
-    x=int(input("enter the size of tumar"))
+    x=float(input("enter the size of tumar"))
     n=np.dot(w,x)+b
     return n
 # cost function 
@@ -19,7 +19,7 @@ def function_cost(x,w,b,y):
     for i in range(l):
         fun=np.dot(x,w)+b
         erro=sigmoid(fun)
-        cost+= -y[i]*np.log(erro)-(1-y[i])*np.log(erro)
+        cost+= -y[i]*np.log(erro)-(1-y[i])*np.log(1-erro)
     cost=cost/l
     return cost
 def grafient_desent(w,b,x,y):
@@ -40,7 +40,7 @@ def gradient_desent(wj,bj,x,y,times,alpha):
         b=b-alpha*jb
         w=w-alpha*jw
     return w,b
-w_final,b_final=gradient_desent(w,0,x_axis,y_axis,1000,1e-7)
+w_final,b_final=gradient_desent(w,0,x_axis,y_axis,1000,0.001)
 print("final w: ",w_final,"final b: ",b_final)
 print("Final cost: ",function_cost(x_axis,w_final,b_final,y_axis))
 e=func(w_final,b_final)
